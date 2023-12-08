b10b0c17b79984f5244ceded27d9a3d186cf102a
melzmatz <melendezdiana95@gmail.com>
2023-12-08 15:50:53 -0700

    viewall

--- northwind-traders-website/scripts/index.js
+++ northwind-traders-website/scripts/index.js
@@ -1,18 +1,30 @@
-let categoryUrl = "http://localhost:3000/categories"
+let categoryUrl = "http://localhost:3000/categories";
 //   http://localhost:3000/products
 
 document.addEventListener("DOMContentLoaded", () => {
-populateSearchDropdown()
+  populateSearchDropdown();
 
+  let dropdown = document.getElementById("searchDropdown");
+
+  dropdown.addEventListener("change", () => {
+    let dropdownValue = dropdown.value;
+
+    if (dropdownValue === "View all") {
+      displayAllProducts();
+    }
   });
-  
-  async function populateSearchDropdown () {
-    let categories = document.getElementById("categorySelect")
+});
+async function populateSearchDropdown() {
+  let categories = document.getElementById("categorySelect");
 
-    const response = await fetch(categoryUrl)
-    const data = await response.json();
-    data.forEach((category) =>{
-        const option = new Option(category.name)
-        categories.appendChild(option)
-    })
-  }
+  const response = await fetch(categoryUrl);
+  const data = await response.json();
+  data.forEach((category) => {
+    const option = new Option(category.name);
+    categories.appendChild(option);
+  });
+}
+
+async function displayAllProducts() {
+  console.log("this works");
+}
